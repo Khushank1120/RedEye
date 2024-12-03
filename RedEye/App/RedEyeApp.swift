485546949b87627636c8c30e0d2e44780cc11038
khushank20 <mistry.khu@northeastern.edu>
2024-12-03 00:58:27 -0500

    Initial Commit

---
+++ RedEye/App/RedEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedEyeApp.swift
+//  RedEye
+//
+//  Created by Khushank on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RedEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ RedEye/Core/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  RedEye
+//
+//  Created by Khushank on 12/3/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, NEU RED Eye")
+    }
+}
+
+#Preview {
+    HomeView()
+}
